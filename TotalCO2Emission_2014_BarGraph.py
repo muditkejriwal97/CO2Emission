ca118bb63862e9b88a8ca02e28101a209d0bad5b
Mudit Kejriwal <muditkejriwal97@gmail.com>
2018-06-27 14:24:30 +0530

    first commit

---
+++ TotalCO2Emission_2014_BarGraph.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter, ScalarFormatter
+
+
+#Reading File and Parsing
+file = 'API_EN.ATM.CO2E.KT_DS2_en_excel_v2_9945482.xls'
+data = pd.ExcelFile(file)
+df1 = data.parse('Data', skiprows=3)
+df1 = df1.drop(["Country Code", "Indicator Name", "Indicator Code"], axis=1)
+df1 = df1.set_index("Country Name")
+#print (df1.loc['India']['2014'])
+df1.loc[["China","United States","India","Russian Federation","Japan"], "2014"].plot(kind='bar')
+
+#Fromatting axis ticks
+plt.ticklabel_format(useOffset=False, style='plain', axis='y')
+
+plt.suptitle('Country wise CO2 Emissions in 2014', fontsize=19)
+plt.xlabel('', fontsize=18)
+plt.ylabel('CO2 Emissions(kt)', fontsize=18)
+
+
+plt.show()
